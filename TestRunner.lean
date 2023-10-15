aba1e49d35c00f71ee25a1b5a867e94c407ef7e9
Jim Fowler <fowler@math.osu.edu>
2023-10-14 23:00:11 -0400

    initial import

--- TestRunner.lean
+++ TestRunner.lean
@@ -1,8 +1,8 @@
 import Homework07
 import Mathlib.Util.AssertNoSorry
 
-assert_no_sorry s07p01
-assert_no_sorry s07p02
-assert_no_sorry s07p03
-assert_no_sorry s07p04
-assert_no_sorry s07p05
+assert_no_sorry s08p01
+assert_no_sorry s08p02
+assert_no_sorry s08p03
+assert_no_sorry s08p04
+assert_no_sorry s08p05
